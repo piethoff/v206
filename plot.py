bd3bd6fd1028373550463d54cc96c311c40822f9
Jan Jaekel <jan.jaekel@tu-dortmund.de>
2017-11-16 14:12:20 +0100

    modified theorie.tex and makefile

--- plot.py
+++ plot.py
@@ -6,15 +6,11 @@ y = x ** np.sin(x)
 
 plt.subplot(1, 2, 1)
 plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+
 
 plt.subplot(1, 2, 2)
 plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+
 
 # in matplotlibrc leider (noch) nicht möglich
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
